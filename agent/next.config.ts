315b7fc1140b8ec6c87515cf0f850d2eda39ac86
Bob German <bgerman1@hotmail.com>
2026-08-09 20:49:56 -0400

    Allow access from other hosts

--- agent/next.config.ts
+++ agent/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
-  /* config options here */
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+
+  allowedDevOrigins: ['local-origin.dev', '*.lab'],
 
 };
+
 export default nextConfig;
